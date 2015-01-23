0c93ee294afb33f28a77482d221d9dece8ad639b
rollick <mark@onepercentclub.com>
2015-01-23 12:00:18 +0100

    Initial commit for reef extras package

---
+++ README.rst
@@ -0,0 +1,5 @@
+====
+Reef Extras
+====
+
+Some extra utils for making a front-end on top of Bluebottle.
---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup
+import reef_extras
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name="django-reef-extras",
+    version=reef_extras.__version__,
+    packages=['reef_extras'],
+    include_package_data=True,
+    license='None',
+    description='A small package for extra utils for a Bluebottle front-end',
+    long_description=README,
+    url="http://onepercentclub.com",
+    author="1%Club Developers",
+    author_email="devteam@onepercentclub.com", 
+    install_requires=[
+        'Django >= 1.6.8',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: None', 
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content'
+    ]
+
+)
+
